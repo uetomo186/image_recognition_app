893e907f6f3fe6737b311d591ad418fb42257a69
tomohiro ueno <hagurono9@gmail.com>
2023-09-21 22:05:11 +0900

    add

--- lib/photo/photo_upload_screen.dart
+++ lib/photo/photo_upload_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -21,8 +20,6 @@ class ImageUploadScreen extends StatefulWidget {
 class _ImageUploadScreenState extends State<ImageUploadScreen> {
   File? _image;
   final picker = ImagePicker();
-  List<Face>? _faces;
-  final FaceDetector faceDetector = FirebaseVision.instance.faceDetector();
 
   Future getImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -33,17 +30,6 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
       } else {
         debugPrint('No image selected.');
       }
-    });
-
-    detectFaces();
-  }
-
-  Future detectFaces() async {
-    FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(_image!);
-    List<Face> faces = await faceDetector.processImage(visionImage);
-
-    setState(() {
-      _faces = faces;
     });
   }
 
@@ -63,15 +49,13 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('画像のアップロード'),
+        title: const Text('写真一覧'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             _image == null ? const Text('画像が選択されていません') : Image.file(_image!),
-            if (_faces != null && _faces!.isNotEmpty)
-              Text('検出された顔の数: ${_faces!.length}'),
             ElevatedButton(
               onPressed: getImage,
               child: const Text('画像を選択'),
