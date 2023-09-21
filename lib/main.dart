69891fb9510f9dbd51e4e4a63c0c1f4074732c70
tomohiro ueno <hagurono9@gmail.com>
2023-09-21 22:05:15 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image_recognition_app/firebase_options.dart';
-import 'package:image_recognition_app/photo/photo_upload_screen.dart';
+import 'package:image_recognition_app/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ImageUploadScreen(),
+      home: const HomePage(),
     );
   }
 }
